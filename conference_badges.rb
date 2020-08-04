0e13be48086e581264fabf7d01bb51b6f05651e4
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-04 14:26:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,6 +11,19 @@ def batch_badge_creator(attendees)
 
 end
 
-#def batch_badge_creator(attendees)
+def assign_rooms(attendees)
+  room = 0
+  attendees.collect do |name|
+    room += 1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
 
-#end
+def printer(attendees)
+  batch_badge_creator(attendees).each do |value|
+    puts value
+  end
+  assign_rooms(attendees).each do |value|
+    puts value
+  end
+end
