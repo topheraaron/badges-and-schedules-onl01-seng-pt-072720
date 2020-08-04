ce24630351d8a85ef8bea5e710969ea78aa0974a
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-04 14:15:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(attendees)
+  array.collect { |name| "Hello, my name is " + name}
+end
+
+#def batch_badge_creator(attendees)
+
+#end
